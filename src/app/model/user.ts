d7637a638b3ff79fb8d4a4026840dc62dbfb353d
papgergo1997 <pap.gergo.1997@gmail.com>
2021-03-08 17:12:40 +0100

    user class created

--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -1,2 +1,13 @@
+import { Location } from './location';
+import { Interest } from './interest';
+
 export class User {
+    id: number = 0;
+    name: string = '';
+    gender: string = '';
+    age: number = 0;
+    photo: string = '';
+    description: string = '';
+    location: Location = new Location();
+    interests: Interest = new Interest();
 }
