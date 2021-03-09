0b991d2874e3c0c2ff8cf2285cf56a930a3012f5
papgergo1997 <pap.gergo.1997@gmail.com>
2021-03-09 13:15:46 +0100

    location service getall() and get() created

---
+++ src/app/service/location.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocationService {
+
+  apiUrl: string = 'http://localhost:3000/locations';
+  list$: BehaviorSubject<Location[]> = new BehaviorSubject<Location[]>([]);
+
+  constructor(private http: HttpClient) { }
+
+
+  getAll(): void {
+    this.http.get<Location[]>(this.apiUrl).subscribe(
+      locations => this.list$.next(locations)
+    );
+  }
+  get(id: number): Observable<Location> {
+    id = typeof id === 'string' ? parseInt(id, 10) : id;
+    const ev: Observable<Location> | undefined = this.http.get<Location>(`${this.apiUrl}/${id}`);
+    if (id == 0) {
+      return of(new Location());
+    } else {
+      return ev;
+    }
+  }
+
+
+}
