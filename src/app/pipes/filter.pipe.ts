aaecb3941c0cd24492f3018d27ab115d47536204
papgergo1997 <pap.gergo.1997@gmail.com>
2021-03-15 19:20:49 +0100

    working filter

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -9,8 +9,7 @@ import { User } from '../model/user';
 export class FilterPipe implements PipeTransform {
 
   user: User = new User()
-  key: string[] = Object.keys(this.user);
-  thisKey: string = 'name';
+  keys: string[] = Object.keys(this.user);
 
   transform(value: any[] | null, phrase: string | number | boolean): any[] | null {
     if (!Array.isArray(value) || !phrase) {
@@ -23,12 +22,12 @@ export class FilterPipe implements PipeTransform {
       if (typeof item === 'number' && typeof phrase === 'number') {
         return item === phrase;
       } else {
-        return ('' + item[this.key[1]]).toLowerCase().includes((phrase as string)) ||
-          ('' + item[this.key[2]]).toLowerCase().includes((phrase as string)) ||
-          ('' + item[this.key[3]]).toLowerCase().includes((phrase as string)) ||
-          ('' + item[this.key[4]]).toLowerCase().includes((phrase as string)) ||
-          ('' + item[this.key[5]]).toLowerCase().includes((phrase as string)) ||
-          ('' + item[this.key[6]]).toLowerCase().includes((phrase as string));
+        return ('' + item[this.keys[1]]).toLowerCase().includes((phrase as string)) ||
+          ('' + item[this.keys[2]]).toLowerCase().includes((phrase as string)) ||
+          ('' + item[this.keys[3]]).toLowerCase().includes((phrase as string)) ||
+          ('' + item[this.keys[4]]).toLowerCase().includes((phrase as string)) ||
+          ('' + item[this.keys[5]]).toLowerCase().includes((phrase as string)) ||
+          ('' + item[this.keys[6]]).toLowerCase().includes((phrase as string));
       }
     })
   }
