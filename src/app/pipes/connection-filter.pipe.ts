6ab563beccc6742cf256ed0f5d1e514430330b55
papgergo1997 <pap.gergo.1997@gmail.com>
2021-03-22 20:52:43 +0100

    connection filter works fine

--- src/app/pipes/connection-filter.pipe.ts
+++ src/app/pipes/connection-filter.pipe.ts
@@ -1,12 +1,40 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../model/user';
+import { UserService } from '../service/user.service';
 
 @Pipe({
   name: 'connectionFilter'
 })
 export class ConnectionFilterPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  currentUser: string = 'aHYQeMCvZD7qV05retF6';
+  some: any;
+  user: User;
+
+  constructor(private useService: UserService) {
+    this.useService.get(this.currentUser).subscribe((user) => this.user = user);
+  }
+
+  transform(value: any[] | null): any[] | null {
+    if (!Array.isArray(value)) {
+      return value;
+    }
+
+    return value.filter((item) => {
+
+      if (this.user.gender == 'male' && (this.user.age > 20 && this.user.age < 30)) {
+        return ('' + item.gender).toLowerCase().includes(('female' as string)) && (item.age < 30 && item.age > 20)
+      } else if (this.user.gender == 'male' && (this.user.age > 30 && this.user.age < 40)) {
+        return ('' + item.gender).toLowerCase().includes(('female' as string)) && (item.age < 40 && item.age > 30)
+      } else if (this.user.gender == 'female' && (this.user.age > 30 && this.user.age < 40)) {
+        return ('' + item.gender).toLowerCase().startsWith(('male' as string)) && (item.age < 40 && item.age > 30)
+      } else if (this.user.gender == 'female' && (this.user.age > 20 && this.user.age < 30)) {
+        return ('' + item.gender).toLowerCase().startsWith(('male' as string)) && (item.age < 30 && item.age > 20)
+      }
+
+    })
+
   }
 
 }
